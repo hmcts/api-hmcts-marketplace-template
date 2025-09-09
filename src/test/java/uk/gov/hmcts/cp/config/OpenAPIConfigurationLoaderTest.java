2adc8f526bec74bd118a7ea041f2eff3899611ae
coling01 <colin.greenwood@hmcts.net>
2025-09-09 16:39:37 +0100

    chore:(openapi): fixup openapi config and test

--- src/test/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoaderTest.java
+++ src/test/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoaderTest.java
@@ -24,7 +24,7 @@ class OpenAPIConfigurationLoaderTest {
         assertEquals("Template API", info.getTitle());
         assertEquals("API description", info.getDescription());
 
-        String apiGitHubRepository = "api-hmcts-marketplace-template";
+        String apiGitHubRepository = "api-cp-template";
         String expectedVersion = System.getProperty("API_SPEC_VERSION", "0.0.0");
         log.info("API version set to: {}", expectedVersion);
 
