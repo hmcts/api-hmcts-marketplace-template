59ef8a4df2bbbf883691ff2bd3b2dd707f937031
coling01 <colin.greenwood@hmcts.net>
2025-09-09 17:42:56 +0100

    chore:(openapi): make swagger url match repo name

--- src/test/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoaderTest.java
+++ src/test/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoaderTest.java
@@ -24,7 +24,7 @@ class OpenAPIConfigurationLoaderTest {
         assertEquals("Common Platform API", info.getTitle());
         assertEquals("API description", info.getDescription());
 
-        String apiGitHubRepository = "api-cp-template";
+        String apiGitHubRepository = "api-hmcts-marketplace-template";
         String expectedVersion = System.getProperty("API_SPEC_VERSION", "0.0.0");
         log.info("API version set to: {}", expectedVersion);
 
